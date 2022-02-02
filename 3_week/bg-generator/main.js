796a2f2af512081785f993eefb4bada27e8fac56
Nguyen Duyen <s2200128@BC-C02GR0BVQ6LR-MacBook-Air.local>
2022-02-02 17:02:09 +0200

    2.2.2022 bg generator and newsletter project

--- 3_week/bg-generator/main.js
+++ 3_week/bg-generator/main.js
@@ -1,43 +1,43 @@
-let form = document.querySelector("form");
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
-let inputs = document.querySelectorAll("input");
-let colorCode = document.querySelector(".color-code");
-
-let direction;
-for (const input of inputs) {
-  input.addEventListener("change", () => {
-    direction = input.value;
-    console.log(direction);
-  });
-}
-
-const setGradient = () => {
-  document.body.style.backgroundImage = `linear-gradient(${direction}, ${color1.value}, ${color2.value})`;
-  colorCode.textContent = `linear-gradient(${direction}, ${color1.value}, ${color2.value})`;
-};
-
-form.addEventListener("change", setGradient);
-
-// In Class solution
 // let form = document.querySelector("form");
 // let color1 = document.querySelector(".color1");
 // let color2 = document.querySelector(".color2");
-// let direction = document.querySelectorAll('input[name="direction"]');
-// console.log(direction);
+// let inputs = document.querySelectorAll("input");
+// let colorCode = document.querySelector(".color-code");
+
+// let direction;
+// for (const input of inputs) {
+//   input.addEventListener("change", () => {
+//     direction = input.value;
+//     console.log(direction);
+//   });
+// }
 
 // const setGradient = () => {
-//   let selectedValue;
-
-//   for (const sel of direction) {
-//     if (sel.checked) {
-//       console.log(sel.value);
-//       selectedValue = sel.value;
-//     }
-//   }
-//   console.log(selectedValue);
-
-//   document.body.style.backgroundImage = `linear-gradient(${selectedValue}, ${color1.value}, ${color2.value})`;
+//   document.body.style.backgroundImage = `linear-gradient(${direction}, ${color1.value}, ${color2.value})`;
+//   colorCode.textContent = `linear-gradient(${direction}, ${color1.value}, ${color2.value})`;
 // };
 
 // form.addEventListener("change", setGradient);
+
+// In Class solution
+let form = document.querySelector("form");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let direction = document.querySelectorAll('input[name="direction"]');
+let colorCode = document.querySelector(".color-code");
+
+const setGradient = () => {
+  let selectedValue;
+
+  for (const sel of direction) {
+    if (sel.checked) {
+      console.log(sel.value);
+      selectedValue = sel.value;
+    }
+  }
+
+  document.body.style.backgroundImage = `linear-gradient(${selectedValue}, ${color1.value}, ${color2.value})`;
+  colorCode.textContent = `linear-gradient (${selectedValue}, ${color1.value}, ${color2.value})`;
+};
+
+form.addEventListener("change", setGradient);
